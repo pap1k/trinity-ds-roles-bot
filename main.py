10da1ffb3405a0363617a9642d419b1bb581defb
pap1k <35275000+pap1k@users.noreply.github.com>
2022-08-31 18:21:51 +0300

    абоба

---
+++ main.py
@@ -0,0 +1,38 @@
+import os
+from tokenize import Name
+import discord
+from discord.utils import get
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+RADIO_CHANNEL = 997523978111438888
+
+
+@client.event
+async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
+    role = client.get_guild(530673741705904150).get_role(1014517727790121030)
+
+    if after.channel != None: #зашел
+        if len(after.channel.members) == 1:
+            print(123)
+            await member.add_roles(role)
+        else:
+            for mem in after.channel.members:
+                print(mem, "removed")
+                await mem.remove_roles(role)
+        if before.channel:
+            if len(before.channel.members) == 1:
+                await before.channel.members[0].add_roles(role)
+
+    else:
+        if len(before.channel.members) == 1:
+            await before.channel.members[0].add_roles(role)
+
+@client.event
+async def on_ready():
+    print(f'Looget in as {client.user}')
+
+client.run(os.getenv("DISCORD_CC"))
