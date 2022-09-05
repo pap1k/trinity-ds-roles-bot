867e6da6a7cfca3a00a6fde89dec5558848e8753
pap1k <35275000+pap1k@users.noreply.github.com>
2022-09-05 15:13:37 +0300

    исключил ботов

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 RADIO_CHANNEL = 997523978111438888
+BOTS_ID = [282859044593598464, 819457016774656060, 797889731731783711, 614109280508968980, 159985870458322944]
 
 
 @client.event
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     role = client.get_guild(530673741705904150).get_role(1014517727790121030)
+    
+    if member.id in BOTS_ID:
+        return
 
     if after.channel != None: #зашел
         if len(after.channel.members) == 1:
