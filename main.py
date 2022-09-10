62df7ce49c78d4571acdf14143ec5306a4592da1
popka <magemaster555@gmail.com>
2022-09-10 21:41:56 +0300

    реагирует на состояние микро

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from itertools import count
 import os
 import discord
 
@@ -26,11 +25,16 @@ def getUser(channel : discord.channel) -> discord.Member:
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     role = client.get_guild(530673741705904150).get_role(1014517727790121030)
     
+    if after.self_mute:
+        await member.add_roles(role)
+    else:
+        await member.remove_roles(role)
+
     if member.bot:
         return
 
     if after.channel != None: #зашел
-        if countUsers(after.channel) == 1:
+        if countUsers(after.channel) == 1 or after.self_mute:
             await member.add_roles(role)
         else:
             for mem in after.channel.members:
