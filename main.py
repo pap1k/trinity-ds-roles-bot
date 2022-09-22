fb04f718a98cca54a9a22fd9a3c433a90f897707
pap1k <35275000+pap1k@users.noreply.github.com>
2022-09-22 15:25:12 +0300

    Почи нил епта

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import discord
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents, proxy="https://proxy.server:3128")
+client = discord.Client(intents=intents)
 
 RADIO_CHANNEL = 997523978111438888
 
 def countUsers(channel : discord.channel):
     c = 0
-    for mem in channel.members:
-        if not mem.bot:
-            c += 1
+    if channel:
+        for mem in channel.members:
+            if not mem.bot:
+                c += 1
     return c
 
 def getUser(channel : discord.channel) -> discord.Member:
@@ -25,15 +26,21 @@ def getUser(channel : discord.channel) -> discord.Member:
 async def on_voice_state_update(member: discord.Member, before: discord.VoiceState, after: discord.VoiceState):
     role = client.get_guild(530673741705904150).get_role(1014517727790121030)
     
+    if after.self_mute:
+        await member.add_roles(role)
+    else:
+        await member.remove_roles(role)
+
     if member.bot:
         return
 
     if after.channel != None: #зашел
-        if countUsers(after.channel) == 1:
+        if countUsers(after.channel) == 1 or after.self_mute:
             await member.add_roles(role)
         else:
             for mem in after.channel.members:
-                await mem.remove_roles(role)
+                if not mem.voice.self_mute:
+                    await mem.remove_roles(role)
         if before.channel:
             if countUsers(before.channel) == 1:
                 await getUser(before.channel).add_roles(role)
